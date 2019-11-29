8f9780a1e04859275ea0d375806bde7c2eee018e
wangdahoo <157195705@qq.com>
2019-11-30 01:02:42 +0800

    chore: remove uesless files

--- src/utils.ts
+++ src/utils.ts
@@ -15,35 +15,3 @@ export const difference = <T> (a: T[], b: T[]): E<T>[] =>
             ),
         []
     )
-
-export const uniqBy = <T> (arr: T[], f: string): T[] => {
-    const r = []
-    const m: any = {}
-
-    for (let i = 0; i < arr.length; i++) {
-        const a: any = arr[i]
-        const k = a[f]
-
-        if (m[k] !== 1) {
-            m[k] = 1
-            r.push(a)
-        }
-    }
-
-    return r
-}
-
-export const pick = (source: any, fileds: string[]) =>
-    fileds.reduce(
-        (result: any, field: string) => ({
-            ...result,
-            ...(source[field] ? { [field]: source[field] } : {})
-        }),
-        {}
-    )
-
-export const omit = (source: any, fields: string[]) =>
-    pick(
-        source,
-        Object.keys(source).filter(k => fields.indexOf(k) === -1)
-    )
